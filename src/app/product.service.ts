94aa5d9bc9a0135b04a50cb89c7be9d2b62aa436
sudeshsubba <sudesh.subba@gmail.com>
2018-10-27 19:34:52 -0500

    Completed module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -5,15 +5,21 @@ import { catchError } from 'rxjs/operators';
 import { Album } from './album';
 import { Track } from './track';
 import { Observable } from 'rxjs/Observable';
+import { Product } from './product';
 
 @Injectable()
 export class ProductService {
   private _albumUrl = '../assets/album.json';
+  private _productsUrl = '../assets/products.json';
 
   constructor(private _http: Http) { }
 
   getAlbum(id: number): Observable<Album> {
     return this._http.get(this._albumUrl).map((response) => <Album>response.json());
+  }
+
+  getProducts(): Observable<Product[]> {
+    return this._http.get(this._productsUrl).map((response) => <Product[]>response.json());
   }
 
 }
